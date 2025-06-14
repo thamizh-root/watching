8471a14cc54f65fae8510dbb47ee9ee363b7d444
thamizh-root <inboxtome26@gmail.com>
2025-06-14 19:28:16 +0530

    all coded

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,31 @@
-import "../global.css";
-import { Text, View } from "react-native";
-import { verifyInstallation } from 'nativewind';
+import { Text, View, ScrollView, Image  } from "react-native";
+
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 export default function Index() {
-  verifyInstallation();
   return (
 
- <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200">Hello, World!</Text>
-    </View>
+	  <View className="flex-1 bg-primary">
+		<Image 
+		source={images.bg} 
+		className="absolute w-full z-0"/>
+	
+	      <ScrollView 
+	      className="flex-11 px-5"
+	      showsVerticalScrollIndictor={false}
+	      contentContainerStyle={{
+	      minHeight: "100%",
+	      paddingBottom: 10
+	      }}>
+
+	      <Image 
+	     source={icons.logo}
+	    className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+      	      </ScrollView>	      
+		
+	</View>
 
   );
 }
